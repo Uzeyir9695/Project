2f2291da6cbdea36b7d0bc06533e40cf252e2e99
Uzeyir9695 <74812496+Uzeyir9695@users.noreply.github.com>
2021-07-26 23:34:56 +0400

    up-to-date

--- public/js/test.js
+++ public/js/test.js
@@ -3,13 +3,6 @@ $(document).ready(function () {
         $(".preloader18, .center").fadeOut(2000);
     })
 
-  /*   $(window).on('scroll', function() {
-        
-        if($(document).scrollTop > 20) 
-        $('body').css('backgroundColor', 'red');
-    
-    }) */
-
     $('a ').click(function() {
         $(this).addClass('.active');
     })
@@ -18,43 +11,7 @@ $(document).ready(function () {
     $('.test').click(function() {
         $(this).css('backgroundColor', 'limeGreen');
     });
-
-    /* if($('.clas').text()==0)
-        $('.fa-thumbs-up').hide(); */
-    
-       /*  $('.clas').text() = 0;
-        $('.like-btn').click(function() {
-            $('.clas').text() =+1;
-        }) */
-
-        /* $('.like-btn').click(function(){
-            let enable = false;
-            if(!enable){
-
-                $('.thum').html('<i class="fas fa-thumbs-down" style="color: rgb(49, 49, 49); font-size: 15px"></i>');
-            } else{
-                $('.thum').html('<i class="fas fa-thumbs-up" style="color: rgb(49, 49, 49); font-size: 15px"></i>');
-            }
-
-        }) */
 });
-
-/* document.getElementsByClassName('like').addEventListener.click(function (){
-       increment();
-   })
-var like = document.getElementsByClassName('clas');
-like.innerHTML = 0;
-function increment() {
-   
-       like.innerHTML+=1;
-   
-} */
-
-/* var input = document.getElementById('likes');
-document.getElementsByClassName('like-btn').addEventListener('click', () => {
-    // input.style.color = 'red';
-    console.log('WOWOOWWO');
-}) */
 
 const likeBtn = document.querySelector('.like-btn');
 let count = document.querySelector('.clas');
